323834bb25c3398c3597fa0b59ba37de3a01e1ad
leandro-ikehara <leandro.ikehara@gmail.com>
2024-11-24 12:18:58 -0300

    Deploy do projeto

--- server.js
+++ server.js
@@ -1,65 +1,10 @@
 import express from "express";
-// import conectarAoBanco from "./src/config/dbConfig.js";
 import routes from "./src/routes/postsRoutes.js";
 
-// console.log(process.env.STRING_CONEXAO)
-// const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
-
-// const posts = [
-//     {
-//         id: 1,
-//         descricao: "Foto de gatinho",
-//         imagem: "https://placecats.com/millie/300/150"
-//       },
-//       {
-//         id: 2,
-//         descricao: "Paisagem montanhosa",
-//         imagem: "https://picsum.photos/seed/mountains/300/200"
-//       },
-//       {
-//         id: 3,
-//         descricao: "Cachorro brincando na praia",
-//         imagem: "https://placedog.net/300/200"
-//       },
-//       {
-//         id: 4,
-//         descricao: "Comida deliciosa!",
-//         imagem: "https://source.unsplash.com/random/300x200/?food"
-//       },
-//       {
-//         id: 5,
-//         descricao: "Arte Abstrata",
-//         imagem: "https://source.unsplash.com/random/300x200/?abstract"
-//       }
-//     ];
-
 const app = express();
-// app.use(express.json());
+app.use(express.static("uploads"))
 routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
-
-// async function getTodosPosts(){
-//   const db = conexao.db("imersao-back-instalike")
-//   const colecao = db.collection("posts")
-//   return colecao.find().toArray()
-
-// }
-
-// app.get("/posts", async (req, res) => {
-//   const posts = await getTodosPosts()
-//   res.status(200).json(posts);
-// });
-
-// function buscarPostPorID(id){
-//     return posts.findIndex((post) => {
-//         return post.id === Number(id)
-//     })
-// }
-
-// app.get("/posts/:id", (req, res) => {
-//     const index = buscarPostPorID(req.params.id)
-//     res.status(200).json(posts[index]);
-// });
--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,5 +1,6 @@
-import {getTodosPosts, criarPost } from "../model/postsModel.js";
+import {getTodosPosts, criarPost, atualizarPost } from "../model/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiServices.js";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts()
@@ -30,6 +31,30 @@ export async function uploadImagem(req, res) {
         const postCriado = await criarPost(novoPost);
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
         fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);
+    }
+    catch(erro){
+        console.error(erro.message);
+        res.status(500).json({"erro":"falha na requisição"});
+    }
+    
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
+
+    try {
+        const imageBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imageBuffer)
+
+        const post = {
+            imgUrl: urlImagem, 
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
+        const postCriado = await atualizarPost(id, post)
         res.status(200).json(postCriado);
     }
     catch(erro){
